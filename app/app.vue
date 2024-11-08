98d8a09fcb86042d99122c5c4a8499d1d077e40c
alexeyzlotnik <alexeyzlotnik@gmail.com>
2024-11-08 17:06:09 +0200

    add gtag

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <script lang="ts" setup>
 useHead({
   titleTemplate: title => title ? `${title} – Free online tools for developers and marketers` : 'Tool Farm – Free online tools for developers and marketers',
+  script: [
+    // Add the Google Analytics script
+    { src: "https://www.googletagmanager.com/gtag/js?id=G-TVCD376VGN", async: true },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TVCD376VGN');
+      `,
+      type: 'text/javascript'
+    }
+  ]
 })
 
 </script>
